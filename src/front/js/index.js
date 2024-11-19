cbc060f530578f7b57c919e4b1196759306988ad
ARMANDA2022 <104578464+ARMANDA2022@users.noreply.github.com>
2024-11-19 00:54:36 +0000

    work in progress

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 import '../styles/index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
+import ShopContextProvider from './store/ShopContext.js'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>,
 );
--- src/front/js/pages/Home.js
+++ src/front/js/pages/Home.js
@@ -1,16 +1,20 @@
-
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-
-import { Context } from "../store/appContext";
+import React from 'react'
+import Hero from '../component/Hero'
+import LatestCollection from '../component/LatestCollection'
+import BestSeller from '../component/BestSeller'
+import OurPolicy from '../component/OurPolicy'
+import NewsletterBox from '../component/NewsletterBox'
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-
+  return (
+    <div>
+      <Hero />
+      <LatestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsletterBox/>
+    </div>
+  )
 }
 
 export default Home
